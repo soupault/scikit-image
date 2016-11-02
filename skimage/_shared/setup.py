8a8093d2fe61e8ee9336c24a2ea56a7e304b9e5e
Egor Panfilov <multicolor.mood@gmail.com>
2016-11-02 12:35:17 +0300

    Updated shared setup.py

--- skimage/_shared/setup.py
+++ skimage/_shared/setup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import os
-
 from skimage._build import cython
 
 base_path = os.path.abspath(os.path.dirname(__file__))
@@ -11,16 +10,16 @@ def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration, get_numpy_include_dirs
 
     config = Configuration('_shared', parent_package, top_path)
-    config.add_data_dir('tests')
+    # config.add_data_dir('tests')
 
     cython(['geometry.pyx'], working_path=base_path)
-    cython(['transform.pyx'], working_path=base_path)
-    cython(['interpolation.pyx'], working_path=base_path)
-
     config.add_extension('geometry', sources=['geometry.c'])
+    cython(['transform.pyx'], working_path=base_path)
     config.add_extension('transform', sources=['transform.c'],
                          include_dirs=[get_numpy_include_dirs()])
+    cython(['interpolation.pyx'], working_path=base_path)
     config.add_extension('interpolation', sources=['interpolation.c'])
+
     return config
 
 
@@ -29,7 +28,7 @@ if __name__ == '__main__':
     setup(maintainer='scikit-image Developers',
           author='scikit-image Developers',
           maintainer_email='scikit-image@python.org',
-          description='Transforms',
+          description='Shared functions',
           url='https://github.com/scikit-image/scikit-image',
           license='SciPy License (BSD Style)',
           **(configuration(top_path='').todict())
